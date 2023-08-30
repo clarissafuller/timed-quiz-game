d105a8ed602bcca9e4e27e9ece86ba78d057719b
clarissafuller <141360959+clarissafuller@users.noreply.github.com>
2023-08-29 22:48:33 -0400

    adds basic css styles and psudo code to javascript

---
+++ assets/script.js
@@ -0,0 +1,34 @@
+//DOM ELEMENTS==========================================
+
+//DATA===================================================
+
+//FUNCTIONS===============================================
+
+//USER INTERACTIONS=======================================
+//user clicks start game
+var startButtonEl = document.querySelector("#start-button");
+var quizWindowEl = document.querySelector("#quiz-window");
+
+startButtonEl.addEventListener("click", function () {
+  display.quizWindowEl;
+});
+//timer starts
+//first question appears
+//user clicks their answer for the first question
+//the second question appears
+//user clicks their answer for the second question
+//the third question appears
+//user clicks their answer for the third question
+//the forth question appears
+//user clicks their answer for the fourth question
+//the final score displays with the initial input
+//user clicks submit to register their initals
+//the high score registers on the high score list
+//user clicks restart game
+//the starting blcok reappears
+//user clicks clear high scores
+//the local sotrage clears the high scores
+//user clicks view high scores
+//the high score list appears
+
+//INITIALIZATIONS==========================================
